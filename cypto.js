98d5a07a2e026ded945546947e67bde0dff74cbb
丁丁 <Kutinging@users.noreply.github.com>
2018-03-04 14:50:36 +0800

    fix bug delete import.js

--- cypto.js
+++ cypto.js
@@ -1,3 +1,42 @@
+/*==============================================================*/
+//At first you need to import encryption method js file which encryption method you want to use.
+Import("md5");
+// Import("pbkdf2");
+// Import("ripemd160");
+// Import("sha1");
+// Import("sha3");
+// Import("sha224");
+// Import("sha256");
+// Import("sha384");
+// Import("hmac-md5");
+// Import("hmac-ripemd160");
+// Import("hmac-sha1");
+// Import("hmac-sha3");
+// Import("hmac-sha224");
+// Import("hmac-sha256");
+// Import("hmac-sha384");
+// Import("hmac-sha512");
+// Import("hmac-sha384");
+// Import("aes");
+// Import("tripledes");
+// Import("rabbit");
+// Import("rc4");
+/*==============================================================*/
+
+/*XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX*/
+/* Here is function block .*/
+function toString(value){
+	value = value.toString();
+	return value;
+}
+function toInt(value){
+	return parseInt(value);
+}
+
+function Import(value){
+	return document.write('<script src="cryptojs/rollups/' + value + '.js"></script>');
+}
+
 function MD5(value){
 	Import("md5");
 	var hash = CryptoJS.MD5(toString(value));
